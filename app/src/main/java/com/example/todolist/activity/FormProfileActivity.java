cb1abfe4f20cd2b98ee5db7784fa081e78efbf62
luckystia <luckystia119@gmail.com>
2021-12-14 18:35:37 +0800

    first commit

---
+++ app/src/main/java/com/example/todolist/activity/FormProfileActivity.java
@@ -0,0 +1,139 @@
+package com.example.todolist.activity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.todolist.R;
+import com.example.todolist.helper.DBHelper;
+import java.util.HashMap;
+
+public class FormProfileActivity extends AppCompatActivity {
+    EditText editTextName;
+    RadioButton radioMale, radioFemale;
+    SeekBar seekBarAge;
+    CheckBox checkBoxPainting, checkboxReading, checkboxSinging;
+    Button buttonSave, buttonBack;
+    TextView textSeekbarAge;
+    String selectedGender, selectedHobby;
+    DBHelper SQLite = new DBHelper(this);
+    boolean isUserAlreadyExist;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_profile);
+
+        editTextName = findViewById(R.id.editName);
+        radioMale = findViewById(R.id.radioMale);
+        radioFemale = findViewById(R.id.radioFemale);
+        textSeekbarAge = findViewById(R.id.textSeekbarValue);
+        seekBarAge = findViewById(R.id.seekBarAge);
+        seekBarAge.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                textSeekbarAge.setText(progress + "");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+
+        checkBoxPainting = findViewById(R.id.checkboxPainting);
+        checkboxReading = findViewById(R.id.checkboxReading);
+        checkboxSinging = findViewById(R.id.checkboxSinging);
+        buttonSave = findViewById(R.id.btnSave);
+        buttonSave.setOnClickListener(v -> {
+            saveProfile();
+        });
+        buttonBack = findViewById(R.id.btn_back);
+        buttonBack.setOnClickListener(v -> {
+            finish();
+        });
+        setUserData();
+    }
+
+    public void saveProfile() {
+        selectedGender = "";
+        if (radioMale.isChecked()) {
+            selectedGender = "Laki-laki";
+        } else if (radioFemale.isChecked()) {
+            selectedGender = "Perempuan";
+        }
+
+        selectedHobby = "";
+        if (checkBoxPainting.isChecked()) {
+            selectedHobby += "Melukis ";
+        }
+        if (checkboxReading.isChecked()) {
+            selectedHobby += "Membaca ";
+        }
+        if (checkboxSinging.isChecked()) {
+            selectedHobby += " Menyanyi ";
+        }
+
+        if (isUserAlreadyExist) {
+            SQLite.updateUser(
+                    editTextName.getText().toString(),
+                    selectedGender,
+                    Integer.parseInt(textSeekbarAge.getText().toString()),
+                    selectedHobby);
+
+        } else {
+            SQLite.insertUser(
+                    editTextName.getText().toString(),
+                    selectedGender,
+                    Integer.parseInt(textSeekbarAge.getText().toString()),
+                    selectedHobby);
+        }
+        finish();
+    }
+
+    public void setUserData() {
+        HashMap<String, String> row = SQLite.getUser();
+        if (!row.isEmpty()) {
+            isUserAlreadyExist = true;
+        }
+        String name = row.get("username") == null ? "" : row.get("username");
+        String gender = row.get("gender") == null ? "" : row.get("gender");
+        String age = row.get("age") == null ? "" : row.get("age");
+        String hobby = row.get("hobby") == null ? "" : row.get("hobby");
+
+        editTextName.setText(name);
+
+        if (gender.equalsIgnoreCase("Laki-laki")) {
+            radioMale.setChecked(true);
+        } else if (gender.equalsIgnoreCase("Perempuan")) {
+            radioFemale.setChecked(true);
+        } else {
+            radioMale.setChecked(false);
+            radioFemale.setChecked(false);
+        }
+
+        if (!age.equals("")) {
+            textSeekbarAge.setText(age);
+            seekBarAge.setProgress(Integer.parseInt(age));
+        }
+
+        if (hobby.contains("Melukis")) {
+            checkBoxPainting.setChecked(true);
+        }
+        if (hobby.contains("Membaca")) {
+            checkboxReading.setChecked(true);
+        }
+        if (hobby.contains("Menyanyi")) {
+            checkboxSinging.setChecked(true);
+        }
+
+    }
+
+}
---
+++ app/src/main/java/com/example/todolist/activity/ProfileActivity.java
@@ -0,0 +1,67 @@
+package com.example.todolist.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.todolist.R;
+import com.example.todolist.helper.DBHelper;
+
+import java.util.HashMap;
+
+public class ProfileActivity extends AppCompatActivity {
+    TextView textName, textAge, textGender, textHobby;
+    Button buttonEditProfile, buttonBack;
+    DBHelper SQLite = new DBHelper(this);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        textName = findViewById(R.id.textName);
+        textGender = findViewById(R.id.textGender);
+        textAge = findViewById(R.id.textAge);
+        textHobby = findViewById(R.id.textHobby);
+        buttonEditProfile = findViewById(R.id.btn_edit_profile);
+        buttonEditProfile.setOnClickListener(v -> {
+            startActivity(new Intent(ProfileActivity.this, FormProfileActivity.class));
+        });
+        buttonBack = findViewById(R.id.btn_back);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        setUserData();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        setUserData();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        finish();
+    }
+
+    public void setUserData() {
+        HashMap<String, String> row = SQLite.getUser();
+        String name = row.get("username") == null ? "" : row.get("username");
+        String gender = row.get("gender") == null ? "" : row.get("gender");
+        String age = row.get("age") == null ? "" : row.get("age");
+        String hobby = row.get("hobby") == null ? "" : row.get("hobby");
+
+        textName.setText("Nama : " + name);
+        textGender.setText("Jenis Kelamin : " + gender);
+        textAge.setText("Umur : " + age);
+        textHobby.setText("Hobi : " + hobby);
+    }
+}
